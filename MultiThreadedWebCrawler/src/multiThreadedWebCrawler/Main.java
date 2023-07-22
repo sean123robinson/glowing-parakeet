9d270a434ff812d8a7f7d5a26619cd180821b29b
sean123robinson <49658884+sean123robinson@users.noreply.github.com>
2023-07-22 02:27:32 -0400

    Update Main.java
    Added terminal user inputs for thread count and starting urls for the crawler to use

--- MultiThreadedWebCrawler/src/multiThreadedWebCrawler/Main.java
+++ MultiThreadedWebCrawler/src/multiThreadedWebCrawler/Main.java
@@ -5,11 +5,32 @@ import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
-		ArrayList<Spider> bots = new ArrayList<>();
-		bots.add(new Spider("https://abcnews.go.com", 1));
-		bots.add(new Spider("https://www.npr.org", 2));
-		bots.add(new Spider("https://www.nytimes.org", 3));
+		Scanner input = new Scanner(System.in);
+		System.out.print("How many sites do you want to launch the crawler on? Enter a number: ");
+		int num_threads = input.nextInt();
+		num_threads = Math.abs(num_threads);
 		
+		String[] urlList = new String[num_threads];
+		
+		for(int i=0; i<num_threads; i++) {
+			System.out.print("Enter a URL ("+(i+1)+" of "+num_threads+"):");
+			urlList[i] = input.next();
+		}
+		
+		System.out.println(Arrays.toString(urlList));
+		
+		input.close();
+		
+		ArrayList<Spider> bots = new ArrayList<>();
+		
+		for(int i=0; i<num_threads; i++) {
+			bots.add(new Spider(urlList[i], i+1));
+		}
+		/*
+		bots.add(new Spider("https://abcnews.go.com", 1));
+		bots.add(new Spider("https://www.npr.org", 2)); 
+		bots.add(new Spider("https://www.nytimes.com", 3));
+		*/
 		for(Spider s : bots) {
 			try {
 				s.getThread().join();
