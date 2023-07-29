790a924d6860b9314eb285e16115882e25e36ce7
sean123robinson <49658884+sean123robinson@users.noreply.github.com>
2023-07-29 17:07:01 -0400

    New Version

---
+++ multiThreadedWebCrawler/Url.java
@@ -0,0 +1,13 @@
+package multiThreadedWebCrawler;
+
+public class Url {
+	private String urlString;
+	
+	public Url(String s) {
+		this.urlString = s;
+	}
+	
+	public String toString() {
+		return this.urlString;
+	}
+}
